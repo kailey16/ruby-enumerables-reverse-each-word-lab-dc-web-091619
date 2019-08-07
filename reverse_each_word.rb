6e229f08c5b3ff0316cdd153224ad02e892d8193
Kyeonga Lee <klee.mcintosh@gmail.com>
2019-08-07 00:47:59 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(sentence)
+  array = sentence.split("")
+  newarray = []
+  array.each do |element|
+    newarray.push(element.reverse)
+    return newarray
+  end
+newarray.join("")
+end
+
+
+def reverse_each_word(sentence)
+  array = sentence.split("")
+  array.collect{|element| element.reverse!}
+  array.join("")
+end
+
