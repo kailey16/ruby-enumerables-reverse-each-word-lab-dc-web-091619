fca43f524adeb2599a38c90401c2bbe74aad51ac
Kyeonga Lee <klee.mcintosh@gmail.com>
2019-08-07 00:56:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   newarray = []
   sentence.split.each do |ele|
-    newarray.push(ele.reverse)
+    newarray.push(ele.reverse!)
     return newarray
   end
   newarray.join("")
